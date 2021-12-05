b63f945cccf32945089a27c540bd917382037371
Shafiul Bashar <dshafiul@gmail.com>
2021-12-06 00:35:28 +0600

    frontend project added again.

---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Employee } from '../models/Employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  
+  baseUri:string = 'http://localhost:4000/api';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private http: HttpClient) { }
+
+  // Create
+  createEmployee(data: Employee): Observable<any> {
+    let url = `${this.baseUri}/create`;
+    return this.http.post(url, data)
+      .pipe(
+        catchError(this.errorMgmt)
+      )
+  }
+
+  // Get all employees
+  getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUri}`).pipe(
+      catchError(this.errorMgmt)
+    );
+  }
+
+  // Get employee
+  getEmployee(id: number): Observable<Employee> {
+    let url = `${this.baseUri}/read/${id}`;
+    return this.http.get<Employee>(url, {headers: this.headers}).pipe(
+      catchError(this.errorMgmt)
+    );
+  }
+
+  // Update employee
+  updateEmployee(id: number, data: Employee): Observable<any> {
+    let url = `${this.baseUri}/update/${id}`;
+    return this.http.put(url, data, { headers: this.headers }).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Delete employee
+  deleteEmployee(id: number): Observable<any> {
+    let url = `${this.baseUri}/delete/${id}`;
+    return this.http.delete(url, { headers: this.headers }).pipe(
+      catchError(this.errorMgmt)
+    )
+  }
+
+  // Error handling 
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
+
+}
